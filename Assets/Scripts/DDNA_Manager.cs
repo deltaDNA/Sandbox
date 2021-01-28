d0a941dfc20eb739989c2e2fe4deb2d96bae3010
Sebastiano <sebastiano.todaro@gmail.com>
2021-01-28 18:33:19 -0500

    Begining of UI for the sandbox demo
    Basic UI for the sandbox game

--- Assets/Scripts/DDNA_Manager.cs
+++ Assets/Scripts/DDNA_Manager.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using DeltaDNA;
-
 public class DDNA_Manager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text btnStartSDK;
+    [SerializeField] private  TMP_Text txtParams;
+    [SerializeField] private TMP_Text txtValues;
+    [SerializeField] private TMP_Text txtEventName;
     // Start is called before the first frame update
     void Start()
     {
+        if (DDNA.Instance.HasStarted)
+        {
+            btnStartSDK.text = "Stop SDK";
+        }
+    }
+    
+   
+    public void OnStartSDKClicked()
+    {
         // Default Configuration points to
         // https://www.deltadna.net/demo-account/sandbox/dev 
-        DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
-        DDNA.Instance.StartSDK(); 
+        //TODO check config use config instead of UI CONFIG
+        if (btnStartSDK.text.StartsWith("Start"))
+        {
+            DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+            DDNA.Instance.StartSDK();
+            DDNA.Instance.AndroidNotifications.RegisterForPushNotifications();
+            btnStartSDK.text = "Stop SDK";
+        }
+        else
+        {
+            DDNA.Instance.StopSDK();
+            btnStartSDK.text = "Start SDK";
+        }
+        
+
     }
+    public void OnRecordEventClick()
+    { 
+        string[] parameters;
+        string[] values;
+        parameters = txtParams.text.Split(';');
+        values = txtValues.text.Split(';');
+        GameEvent gameEvent = null;
 
-
+        for (int i = 0; i < parameters.Length-1; i++)
+        {
+            if(i==0)
+                gameEvent = new GameEvent(txtEventName.text);
+            gameEvent.AddParam(parameters[i], values[i]);
+        }
+          
+        DDNA.Instance.RecordEvent(gameEvent);
+    }
 }
---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PanelManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text subTitle;
+
+    [Header("Panel Objects")]
+    [SerializeField] private GameObject panelSetup;
+    [SerializeField] private GameObject panelEvents;
+    [SerializeField] private GameObject panelMain;
+    [SerializeField] private GameObject panelADS;
+    [SerializeField] private GameObject panelIAP;
+    [SerializeField] private GameObject panelCampaigns;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivatePanel(panelMain.name);
+        subTitle.text = "menu";
+    }
+       
+    void ActivatePanel(string panel)
+    {
+        panelMain.SetActive(panel.Equals(panelMain.name));
+        panelSetup.SetActive(panel.Equals(panelSetup.name));
+        panelEvents.SetActive(panel.Equals(panelEvents.name));
+        panelADS.SetActive(panel.Equals(panelADS.name));
+        panelIAP.SetActive(panel.Equals(panelIAP.name));
+        panelCampaigns.SetActive(panel.Equals(panelCampaigns.name));
+    }
+    public void OnEventsClick()
+    {
+        ActivatePanel(panelEvents.name);
+        subTitle.text = "events";
+
+    }
+    public void OnConfigClick()
+    {
+        ActivatePanel(panelSetup.name);
+        subTitle.text = "configuration";
+    }
+    public void OnBackClick()
+    {
+        ActivatePanel(panelMain.name);
+        subTitle.text = "menu";
+    }
+
+    public void OnADSClick()
+    {
+        ActivatePanel(panelADS.name);
+        subTitle.text = "ADS";
+    }
+
+    public void OnCampaignsClick()
+    {
+        ActivatePanel(panelCampaigns.name);
+        subTitle.text = "Campaigns";
+    }
+
+    public void OnIAPClick()
+    {
+        ActivatePanel(panelIAP.name) ;
+        subTitle.text = "IAP";
+    }
+
+
+
+}
