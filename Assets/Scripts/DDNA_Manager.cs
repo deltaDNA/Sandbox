72db5fec6739a1ae223d39cfa48093e9bbb71322
Laurie McCulloch <laurie.mcculloch@ntlworld.com>
2021-01-28 18:29:57 +0000

    DeltaDNA SDK v5.0.1 Added

---
+++ Assets/Scripts/DDNA_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA;
+
+public class DDNA_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Default Configuration points to
+        // https://www.deltadna.net/demo-account/sandbox/dev 
+        DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+        DDNA.Instance.StartSDK(); 
+    }
+
+
+}
